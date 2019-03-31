45a58d89c7778c539cec45f013ffeb07d6ee08fc
Amber Ko <koamberko@gmail.com>
2019-03-31 15:59:22 -0700

    Create skillset.php

---
+++ skillset.php
@@ -0,0 +1,23 @@
+<?php include("header.php") ?>
+
+
+<body>
+  <div class="container">
+    <div class="hmc">
+    	<h1>Hot Metal Composition</h1>
+    </div>
+    <div class="hmco">
+    	<h1>Hot Metal Composition</h1>
+    </div>
+    <div>
+    	<hr class="line" width="4" size="350">
+    </div>
+    <div class="hmct">
+    	<p>In printing and typography, hot metal typesetting (also called mechanical typesetting, hot lead typesetting, hot metal, and hot type) is a technology for typesetting text in letterpress printing. This method injects molten type metal into a mold that has the shape of one or more glyphs. The resulting sorts or slugs are later used to press ink onto paper. Normally the typecasting machine would be controlled by a keyboard or by a paper tape.
+
+    	Hot metal typesetting was developed in the late nineteenth century as a development of conventional cast metal type.[1] The technology had several advantages: it reduced labour since type sorts did not need to be slotted into position manually, and cast crisp new type for each printing job. In the case of Linotype machines, each line was cast as a robust continuous block (hence "line o'type") which was useful for rapid newspaper printing. It was the standard technology used for mass-market printing from the late nineteenth century, finally declining with the arrival of phototypesetting and then electronic processes in the 1950s to 1980s.</p>
+  	</div>
+  </div>
+
+<?php include("bluefooter.php") ?>
+
